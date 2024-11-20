1bc8a11ac93e9ff6eb56ad665528f5d374cd01f5
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-20 13:35:19 +0100

    feladat

--- 01_lista_elemeinek_megszamolasa.py
+++ 01_lista_elemeinek_megszamolasa.py
@@ -4,3 +4,10 @@ Határozd meg a lista hosszát egy ciklus segítségével
 (a len() függvény megoldásán kívül használj for ciklusos megoldást is),
 és írd ki!
 """
+számok = [5, 8, 12, 15, 22]
+print(len(számok))
+
+szám_index = 1
+for szám in számok:
+    print(f"{szám_index} {szám}")
+    szám_index += 1
