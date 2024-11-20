31eda4e66908a25ed4435bbf6c25d0d8ba28336d
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-11-20 13:39:40 +0100

    kettes

--- 02_lista_elemeinek_szorzata.py
+++ 02_lista_elemeinek_szorzata.py
@@ -2,4 +2,8 @@
 Hozz létre egy listát számokkal: [2, 4, 6, 8]. 
 Írd ki a lista elemeinek szorzatát (azaz a számok szorzatát)!
 """
-szamok = []
+szorzat = 1
+szamok = [2, 4, 6, 8]
+for szam in szamok:
+    szorzat *= szam
+print(szorzat)
